7e91200cd429d683dd9bbabd8d4c0ddd60b12974
mrgdevelops <mrgdevelops@gmail.com>
2024-10-13 11:48:03 +0200

    Second commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+@charset "utf-8";
+/* CSS Document */
+document.getElementById("myButton").addEventListener("click", function() {
+    document.getElementById("message").textContent = "Button clicked!";
+});
